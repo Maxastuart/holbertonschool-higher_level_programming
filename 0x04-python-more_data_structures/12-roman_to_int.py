62457e22c951b30960d65aa23e9a86c421f6ce06
maxastuart <maxastuart@gmail.com>
2019-09-19 18:39:17 +0000

    Improved 12-roman to Pass Minimum Tests

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,18 @@
 def roman_to_int(roman_string):
     result=0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    '''for each sequential letter in the string add it all up'''
-    for c in roman_string:
-        result += romans[c]
+
+    if len(roman_string) is None:
+        return
+    elif len(roman_string) == 1:
+        return romans[roman_string]
+    '''make a list of each letter in the string and the one following it'''
     for i, n in zip(roman_string, roman_string[1:]):
-        if i < n:
-            result -= i
+        if romans[i] >= romans[n]:
+            result += romans[i]
+        else:
+            result -= romans[i]
+    '''add the last letter of the string which couldn't be zip'd'''
+    result += romans[roman_string[-1]]
+
     return result
