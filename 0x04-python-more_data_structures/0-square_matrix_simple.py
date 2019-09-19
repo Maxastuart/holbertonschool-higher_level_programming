a9f732a63ab876b87d5571665c53bcadd236d084
maxastuart <maxastuart@gmail.com>
2019-09-19 17:56:27 +0000

    Update 0-square to check if matrix is matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        return [[i * i for i in row] for row in matrix]
+        if isinstance(matrix, list):
+            if all(isinstance(elem, list) for elem in matrix):
+                return [[i * i for i in row] for row in matrix]
