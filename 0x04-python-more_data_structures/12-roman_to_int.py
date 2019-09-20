0e5c17504570e8790238bff29c6b03cff79eb462
maxastuart <maxastuart@gmail.com>
2019-09-20 04:42:30 +0000

    Add Check for repeat_count After For Loop, Rename repeat_count for Clarity, & Revise Commenting Style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,10 @@
 def roman_to_int(roman_string):
     result = 0
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    current_roman_count = 0
+    repeat_count = 0
 
+    if roman_string is None:
+        return 0
     if len(roman_string) is None:
         return 0
     if not isinstance(roman_string, str):
@@ -14,29 +16,30 @@ def roman_to_int(roman_string):
     if len(roman_string) == 1:
         return romans[roman_string]
 
-    '''
-    Make a list of each letter in the string and the one following it.
-    If the current roman numeral is repeated before a larger \
-    roman numeral, return 0.
-    '''
+    # Make a list of each letter in the string and the one following it.
+    # If the current roman numeral is repeated before a larger roman numeral,
+    # return 0.
     for i, n in zip(roman_string, roman_string[1:]):
 
         if romans[i] > romans[n]:
             result += romans[i]
-            current_roman_count = 0
+            repeat_count = 0
 
         elif romans[i] is romans[n]:
-            if current_roman_count > 3:
+            if repeat_count > 3:
                 return 0
-            current_roman_count += 1
             result += romans[i]
-            '''otherwise roman[i] < roman[n]'''
-        else:
-            if current_roman_count > 0:
+            repeat_count += 1
+
+        else:  # romans[i] < romans[n]:
+            if repeat_count > 0:
                 return 0
             result -= romans[i]
+            repeat_count = 0
 
-    '''add the last letter of the string which couldn't be zip'd'''
+    # add the last letter of the string which couldn't be zip'd
+    if repeat_count > 3:
+        return 0
     result += romans[roman_string[-1]]
 
     return result
