4b13d97347b63465b0db35fa0580f4d2565de9f6
maxastuart <maxastuart@gmail.com>
2019-09-20 04:08:24 +0000

    Changed Check for I and Repeating Roman Numbers to Reflect Natural Use

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,7 +16,7 @@ def roman_to_int(roman_string):
 
     '''
     Make a list of each letter in the string and the one following it.
-    If the current roman numeral is not I:1 and is repeated before a larger \
+    If the current roman numeral is repeated before a larger \
     roman numeral, return 0.
     '''
     for i, n in zip(roman_string, roman_string[1:]):
@@ -26,16 +26,14 @@ def roman_to_int(roman_string):
             current_roman_count = 0
 
         elif romans[i] is romans[n]:
-            if current_roman_count > 1:
+            if current_roman_count > 3:
                 return 0
-            if romans[i] is not romans['I']:
-                current_roman_count += 1
+            current_roman_count += 1
             result += romans[i]
             '''otherwise roman[i] < roman[n]'''
         else:
-            if romans[i] is not romans['I']:
-                if current_roman_count > 1:
-                    return 0
+            if current_roman_count > 0:
+                return 0
             result -= romans[i]
 
     '''add the last letter of the string which couldn't be zip'd'''
