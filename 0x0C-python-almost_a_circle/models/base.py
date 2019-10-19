f41e36a5753506c3c905bb0bfe3f323c6a69e135
maxastuart <maxastuart@gmail.com>
2019-10-19 17:37:24 +0000

    15 to json str & 16 with write

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+
+
 """ Base class module
 """
 
@@ -15,3 +18,23 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries.
+        list_dictionaries: a list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) < 1:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+        list_objs: a list of instances who inherits of Base.
+        """
+        with open(cls.__name__ + ".json", 'w', encoding="UTF-8") as myfile:
+            mylist = []
+            for obj in list_objs:
+                mylist.append(obj.to_dictionary())
+            myfile.write(cls.to_json_string(mylist))
