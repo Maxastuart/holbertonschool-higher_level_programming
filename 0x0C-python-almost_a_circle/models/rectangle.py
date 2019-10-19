d9e900ac65973d27b9d4e32a5fcae10ed5290fc4
maxastuart <maxastuart@gmail.com>
2019-10-19 04:43:51 +0000

    8 create .update(*args)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -89,12 +89,25 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
 
     def area(self):
-        """Return the area of the rectangle"""
+        """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def display(self):
-        """Prints the rectangle with #s"""
+        """Prints the rectangle with #s."""
         for n in range(self.__y):
             print("")
         for row in range(self.__height):
             print(' ' * self.__x + '#' * self.__width)
+
+    def update(self, *args):
+        """Updates the attributes with the given values."""
+        if args is not None:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
