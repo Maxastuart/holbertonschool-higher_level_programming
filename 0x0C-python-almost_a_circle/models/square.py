b2db4d00896062193ea6b700e4075dae5445c4f3
maxastuart <maxastuart@gmail.com>
2019-10-19 06:38:13 +0000

    14 square to dict

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,6 @@ class Square(Rectangle):
         elif kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return dict(id=self.id, size=self.size, x=self.x, y=self.y)
