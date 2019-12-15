2b91641f50cb8103d2de9d71e185210cb0db41bf
maxastuart <maxastuart@gmail.com>
2019-12-15 01:11:34 +0000

    Change SQL back, remove charset

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,10 +7,10 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                           passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
