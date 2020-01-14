9d20c7f9277d21d9d76e9b48615b0bac196cb3d5
Max Stuart <maxastuart@gmail.com>
2020-01-14 07:47:57 +0000

    Update 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,4 +2,4 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-    print(requests.post(argv[1], data={'email': argv[2]}))
+    print(requests.post(argv[1], data={'email': argv[2]}).text)
