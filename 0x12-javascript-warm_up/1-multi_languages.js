9312e895d809f7500fbb8b6d6226601e68e2d37a
Max Stuart <maxastuart@gmail.com>
2020-02-04 22:41:07 +0000

    added first missing semicolon <3 javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 'use strict';
 let words = 'C is fun';
 console.log(words);
-words = 'Python is cool'
+words = 'Python is cool';
 console.log(words);
 words = 'Javascript is amazing';
 console.log(words);
