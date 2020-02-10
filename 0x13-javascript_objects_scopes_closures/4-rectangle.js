803c573680f721ed7912678c72ec311da17edc12
Max Stuart <maxastuart@gmail.com>
2020-02-10 23:45:07 +0000

    Create 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+'use strict';
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let line = '';
+    for (let i = 0; i < this.width; i++) {
+      line += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
